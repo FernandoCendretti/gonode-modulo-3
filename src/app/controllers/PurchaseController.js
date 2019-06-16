dd7917770c9e0aa2e5c38247d1ca900940d999c2
FernandoCendretti <luiz.cendretti@gmail.com>
2019-06-15 23:50:18 -0300

    Adicionando Template de email

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -3,17 +3,23 @@ const User = require('../models/User')
 const Mail = require('../services/Mail')
 
 class PurchaseController {
+  /**
+   * Função que dispara um email, quando há uma solicitação de compras
+   * @param {express.Request} req
+   * @param {express.Response} res
+   */
   async store (req, res) {
     const { ad, content } = req.body
 
     const purchaseAd = await Ad.findById(ad).populate('author')
-    const user = await User.findById
+    const user = await User.findById(req.userId)
 
     await Mail.sendMail({
       from: '"Fernando Cendretti" <luiz.cendretti@gmail.com>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra:${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -1,6 +1,26 @@
 const nodemailer = require('nodemailer')
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars')
+const exphbs = require('express-handlebars')
 const mailConfig = require('../../config/mail')
 
 const transport = nodemailer.createTransport(mailConfig)
+
+const viewPath = path.resolve(__dirname, '..', 'views', 'emails')
+/**
+ * Midlleware que configura o jeito que o nodemailer vai entender os templates
+ * de email
+ */
+transport.use(
+  'compile',
+  hbs({
+    viewEngine: exphbs.create({
+      partialsDir: path.resolve(viewPath, 'partials'),
+      defaultLayout: null
+    }),
+    viewPath,
+    extName: '.hbs'
+  })
+)
 
 module.exports = transport
