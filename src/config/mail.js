d26612afa201f24f0522f2377ccbfc6f445e5c9b
FernandoCendretti <luiz.cendretti@gmail.com>
2019-06-24 21:13:02 -0300

    Variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '1ee7497e4e912c',
-    pass: 'c9aed7c1606f01'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://e384f656b4724c798e75913fa05f53ad@sentry.io/1487090'
+  dsn: process.env.SENTRY_DSN
 }
